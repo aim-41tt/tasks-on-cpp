3439244ff3312fde4dcbf204d8e6ff5bd1126ff1
aim_41tt <0505mustik@gmail.com>
2024-09-14 13:02:39 +0300

    fixed z2/

--- z2/z8.cpp
+++ z2/z8.cpp
@@ -1,13 +1,78 @@
-// 7: Алгоритм с переменной типа int
-
 #include <iostream>
+#include <cmath> 
+
 using namespace std;
 
+int factorial(int n) {
+    if (n < 0) {
+        throw invalid_argument("Факториал определен только для неотрицательных чисел.");
+    }
+    int result = 1;
+    for (int i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
+}
+
 int main() {
-    int a = 10;
-    a++;
-    a -= 3;
-    a %= 2;
-    cout << a << endl;
+    char operation;
+    int num1, num2;
+    bool exit = false;
+
+    while (!exit) {
+        cout << "Выберите операцию (+, -, *, /, ^, ! или q для выхода): ";
+        cin >> operation;
+
+        if (operation == 'q') {
+            exit = true;
+            continue;
+        }
+
+        if (operation != '!') {
+            cout << "Введите первое число: ";
+            cin >> num1;
+
+            cout << "Введите второе число: ";
+            cin >> num2;
+
+            switch (operation) {
+                case '+':
+                    cout << "Результат: " << num1 + num2 << endl;
+                    break;
+                case '-':
+                    cout << "Результат: " << num1 - num2 << endl;
+                    break;
+                case '*':
+                    cout << "Результат: " << num1 * num2 << endl;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        cout << "Результат: " << num1 / num2 << endl;
+                    } else {
+                        cout << "Ошибка: Деление на ноль невозможно!" << endl;
+                    }
+                    break;
+                case '^':
+                    cout << "Результат: " << pow(num1, num2) << endl;
+                    break;
+                default:
+                    cout << "Неизвестная операция." << endl;
+            }
+        }
+        else {
+            cout << "Введите число для вычисления факториала: ";
+            cin >> num1;
+
+            try {
+                int factoriale = factorial(num1);
+                cout << "Результат: " << factoriale << endl;
+            } catch (const invalid_argument& e) {
+                cout << "Ошибка: " << e.what() << endl;
+            }
+        }
+
+        cout << endl;
+    }
+
     return 0;
 }
